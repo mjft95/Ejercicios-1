ed273be4d02c2cc1b122ea13abaf1b24d99dca1d
CORP\mjflores <mariajose.florestorne@capgemini.com>
2022-12-02 21:26:52 +0100

    Ejercicio 3

---
+++ Ejercicio 3/index.js
@@ -0,0 +1,14 @@
+const numero1 = 100;
+const numero2 = 10;
+
+const suma = numero1 + numero2;
+console.log (suma);
+
+const resta = numero1 - numero2;
+console.log (resta);
+
+const multplicacion = numero1 * numero2;
+console.log (multplicacion);
+
+const division = numero1 / numero2;
+console.log (division);
