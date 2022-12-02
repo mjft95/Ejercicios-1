5685db5c2681c064f5aaa7113744155b16d4393d
CORP\mjflores <mariajose.florestorne@capgemini.com>
2022-12-02 21:00:02 +0100

    Ejercicios

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,28 @@
+
+//Crea una variable llamada nombre y asígnale tu nombre como valor.
+const nombre = "MJ";
+
+//Crea una variable llamada edad y asígnale tu edad como valor.
+const edad = 27;
+
+//Crea una variable llamada mayorDeEdad y asígnale un valor booleano que indique si eres mayor de edad.
+const mayorDeEdad = true;
+
+//Crea una variable llamada direccion y asígnale un objeto con tu dirección.
+const direccion = {
+    calle : "Calle falsa",
+    numero : 24,
+    codigoPostal : 28040, 
+};
+
+//Crea una variable llamada coloresFavoritos y asígnale un array con tus colores favoritos.
+const coloresFavoritos = ["amarillo", "verde", "azul", "blanco"];
+
+//Crea una variable llamada lenguajesFavoritos y asígnale un array con tus lenguajes de programación favoritos.
+const lenguajesFavoritos = ["JavaScript", "Python", "Java"];
+
+//Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos.
+const mejorLenguaje = lenguajesFavoritos [0];
+
+//Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos.
+const peorLenguaje = lenguajesFavoritos [lenguajesFavoritos.length-1];
